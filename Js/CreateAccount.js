a1abd129a1ac8e7dfe6c321fa50f70359cfc72fa
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-03 00:20:36 +0200

    prepare Create-new-account

---
+++ Js/CreateAccount.js
@@ -0,0 +1,9 @@
+
+var CreateNewAccount = document.getElementById("cancel-new-account");
+
+if(CreateNewAccount){
+    CreateNewAccount.onclick = function(){
+        location.href = "/Index.html?login";
+    }
+}
+
