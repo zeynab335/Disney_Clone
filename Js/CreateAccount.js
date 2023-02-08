a4e84c005b931e6c2239448a1d7285672984215d
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-08 03:43:47 +0200

    add Register

--- Js/CreateAccount.js
+++ Js/CreateAccount.js
@@ -9,36 +9,59 @@ if(CreateNewAccount){
 
 
 var AccountPath = "../../Assets/Images/Account/"
-var carouselRegister = document.getElementById('carousel-inner-register');
-var carouselItem1 = document.getElementById('carousel-item-1');
-var carouselItem2 = document.getElementById('carousel-item-2');
-var carouselItem3 = document.getElementById('carousel-item-3')
-
+var carouselRegister = document.querySelector('.carousel-items-register');
 
 if(carouselRegister){
 
-    for(var i=1 ; i<=5 ; i++ ){
+
+    for(let i=1 ; i<=15 ; i++ ){
         var element1 = document.createElement('img');
+       
         element1.setAttribute('src',`${AccountPath}${i}.png`);
-        element1.setAttribute('class',`d-block`);
-        carouselItem1.append(element1);
+        // element1.setAttribute('class',`d-block`);
+        carouselRegister.append(element1);
     }
     
-      
-    for(var j=6 ; j<=11 ; j++ ){
-        var element2 = document.createElement('img');
-        element2.setAttribute('src',`${AccountPath}${j}.png`);
-        element2.setAttribute('class',`d-block`);
-        carouselItem2.append(element2);
-    }
-
-    for(var j=11 ; j<=15 ; j++ ){
-        var element3 = document.createElement('img');
-        element3.setAttribute('src',`${AccountPath}${j}.png`);
-        element3.setAttribute('class',`d-block`);
-        carouselItem3.append(element3);
-    }
     
 
     
 }
+
+
+// active image
+var images = document.querySelectorAll('.carousel-items-register img');
+var inner = document.querySelector('.carousel-items-register')
+images[3].classList.add('active');
+
+
+images.forEach((e)=>{
+    
+    e.onclick = () =>{
+        
+        var index = Array.from(e.parentNode.children).findIndex((e)=>e.classList.contains('active'));
+        
+        if(images[index].getBoundingClientRect().left > e.getBoundingClientRect().left ){
+            inner.scrollLeft -= 160
+
+        }
+        else inner.scrollLeft += 160
+        
+        Array.from(e.parentNode.children).forEach((e)=>{
+            e.classList.remove("active");
+        })
+        e.classList.add("active");
+
+
+        document.querySelector('.Register-arrows .arrow-right').addEventListener('click', () => {
+            inner.scrollLeft += 100
+            console.log(inner.scrollLeft)
+        })
+
+        document.querySelector('.Register-arrows .arrow-left').addEventListener('click', () => {
+            inner.scrollLeft -= 100;
+            console.log(inner.scrollLeft)
+        })
+    }
+
+})
+
--- Js/LoadFiles.js
+++ Js/LoadFiles.js
@@ -2,15 +2,12 @@
 $(function(){
     $('#Index').load('./Html/Login/Login.html');
     
-    // $('#Header').load('./Html/Header.html');
+    $('#Header').load('./Html/Header.html');
 })
-
-
 
 
 var isCreatedAccount = location.search == "?create-account";
 if(isCreatedAccount){
-    console.log("dd")
     window.addEventListener("load", function(){
         $('#Index').load('./Html/Login/CreateProfile.html');
     });
@@ -19,8 +16,9 @@ if(isCreatedAccount){
 
 var isCreatedAccount = location.search == "?login";
 if(isCreatedAccount){
-    console.log("dd")
     window.addEventListener("load", function(){
         $('#Index').load('./Html/Login/Login.html');
     });
 }
+
+
