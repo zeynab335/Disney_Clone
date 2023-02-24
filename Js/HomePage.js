e6934a253a115c4737de7c378cdabf59301c28a6
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-24 23:02:56 +0200

    finishing categories section

--- Js/HomePage.js
+++ Js/HomePage.js
@@ -2,5 +2,7 @@
 $(function(){
     $('#header').load('./Html/HomePage/Header.html');
     $('#categories').load('./Html/HomePage/Categories.html');
+    $('#products').load('./Html/Products/Products.html');
+
 
 });
